60500c607dc8102ec7b650e54503f5d3a7179f0d
Henriquelmeeee <smtpwebhost@gmail.com>
2023-08-21 23:57:45 -0300

    Criando primeiros objetos para a AST

--- src/AST/Main.h
+++ src/AST/Main.h
@@ -2,12 +2,42 @@
 
 #include "../Forward.h"
 #include "../Interpreter/Interpreter.h"
+
 #include "Value.h"
+
+class Scope;
 
 class ASTNode {
   public:
-    virtual std::string class_name() { return "AST Node"; }
+    virtual std::string class_name() { return "ASTNode"; }
 
-    virtual Value execute();
+    virtual Value execute(Scope* s);
+};
+
+class Identifier {
+  public:
+};
+
+class VariableDeclaration;
+
+class Scope {
+  public:
+    std::vector<VariableDeclaration*> s_variables;
+};
+
+class VariableDeclaration : public ASTNode {
+  public:
+    virtual std::string class_name() { return "VariableDeclaration"; }
+    
+    Identifier name;
+    Value value;
+
+    VariableDeclaration(Identifier _name, Value _value) : name(_name), value(_value) {
+    }
+
+    virtual Value execute(Scope* s) {
+
+    }
+
 
 };
